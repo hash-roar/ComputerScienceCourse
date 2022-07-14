c4fe7555e6929ca8a927ba7dc2e51f62d4b05c4e
hash-roar <1022647354@qq.com>
2022-07-14 18:36:58 +0800

    add clap package and pass some tests

--- talent-plan/courses/rust/my_projects/kvs/src/bin/kvs.rs
+++ talent-plan/courses/rust/my_projects/kvs/src/bin/kvs.rs
@@ -1,4 +1,30 @@
+use clap::{arg, command, Command};
 
-fn main(){
-    println!("hello world");
+fn main() {
+    let mathes = command!()
+        .propagate_version(true)
+        .subcommand(
+            Command::new("set")
+                .about("Set Value in the kvs")
+                .arg(arg!([key]))
+                .arg(arg!([value])),
+        )
+        .subcommand(
+            Command::new("get")
+                .about("get value by key")
+                .arg(arg!([key])),
+        )
+        .subcommand(
+            Command::new("rm")
+                .about("remove key from kvs")
+                .arg(arg!([key])),
+        )
+        .get_matches();
+
+    if let Some(maches) = mathes.subcommand_matches("set") {
+        let key: &String = maches.get_one("key").expect("expect key");
+        let value:&String = maches.get_one("value").expect("expect value");
+
+        println!("{}  {}",key,value)
+    }
 }
